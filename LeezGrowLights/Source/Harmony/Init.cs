264dbcc4a426ec40263074045375d8aabb220cd5
JpegIsHere <136973495+JpegIsHere@users.noreply.github.com>
2026-08-07 21:03:08 +1000

    dev: bump runtime banner to v0.5.3-dev3

--- LeezGrowLights/Source/Harmony/Init.cs
+++ LeezGrowLights/Source/Harmony/Init.cs
@@ -13,7 +13,7 @@ namespace LeezGrowLights
         {
             try
             {
-                LeezLog.Info("Loading V3.1 grow-light runtime candidate v0.5.3-dev2");
+                LeezLog.Info("Loading V3.1 grow-light runtime candidate v0.5.3-dev3");
                 Harmony harmony = new Harmony(_modInstance.Name);
                 PatchInstaller.Install(harmony);
             }
