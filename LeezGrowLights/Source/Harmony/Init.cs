8f86f5d5b941995e7a5186f78d5f148e2ab831ab
JpegIsHere <136973495+JpegIsHere@users.noreply.github.com>
2026-08-07 23:51:28 +1000

    feat: enable colour-system runtime hooks

--- LeezGrowLights/Source/Harmony/Init.cs
+++ LeezGrowLights/Source/Harmony/Init.cs
@@ -9,14 +9,15 @@ namespace LeezGrowLights
         {
             try
             {
-                LeezLog.Info("Loading V3.1 grow-light runtime candidate v0.5.3-dev5");
+                LeezLog.Info("Loading V3.1 grow-light runtime candidate v0.7.0-dev1");
                 Harmony harmony = new Harmony(_modInstance.Name);
                 PatchInstaller.Install(harmony);
                 BlockRemovalInstaller.Install(harmony);
+                GrowLightColourInstaller.Install(harmony);
             }
             catch (Exception ex)
             {
-                LeezLog.Error("Runtime initialisation failed. Grow-light acceleration will remain disabled.\n" + ex);
+                LeezLog.Error("Runtime initialisation failed. Grow-light runtime will remain partially disabled.\n" + ex);
             }
         }
     }
