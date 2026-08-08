f30c03d2cb3848acee33dbde16b260131474660f
JpegIsHere <136973495+JpegIsHere@users.noreply.github.com>
2026-08-08 18:02:47 +1000

    chore: advance colour runtime to dev2

--- LeezGrowLights/Source/Harmony/Init.cs
+++ LeezGrowLights/Source/Harmony/Init.cs
@@ -9,7 +9,7 @@ namespace LeezGrowLights
         {
             try
             {
-                LeezLog.Info("Loading V3.1 grow-light runtime candidate v0.7.0-dev1");
+                LeezLog.Info("Loading V3.1 grow-light runtime candidate v0.7.0-dev2");
                 Harmony harmony = new Harmony(_modInstance.Name);
                 PatchInstaller.Install(harmony);
                 BlockRemovalInstaller.Install(harmony);
