5ab2564d37173f594bf74e35c3211e2967e9fa73
JpegIsHere <136973495+JpegIsHere@users.noreply.github.com>
2026-08-09 14:07:12 +1000

    Resync growlight power after tile deserialization

--- LeezGrowLights/Source/Harmony/GrowLightPowerPatches.cs
+++ LeezGrowLights/Source/Harmony/GrowLightPowerPatches.cs
@@ -68,6 +68,17 @@ namespace LeezGrowLights
                 installed++;
             }
 
+            MethodInfo onReadComplete =
+                AccessTools.Method(typeof(TileEntity), "OnReadComplete");
+            if (onReadComplete != null)
+            {
+                harmony.Patch(
+                    onReadComplete,
+                    postfix: new HarmonyMethod(
+                        typeof(GrowLightPowerPatches), nameof(OnReadCompletePostfix)));
+                installed++;
+            }
+
             if (installed == 0)
                 LeezLog.Warning("Grow-light power-draw hooks were not found in the V3.1 runtime.");
             else
@@ -92,6 +103,12 @@ namespace LeezGrowLights
         }
 
         public static void InitializePowerDataPostfix(TileEntityPowered __instance)
+        {
+            if (__instance is TileEntityPoweredBlock tile)
+                Synchronize(tile, tile.IsToggled);
+        }
+
+        public static void OnReadCompletePostfix(TileEntity __instance)
         {
             if (__instance is TileEntityPoweredBlock tile)
                 Synchronize(tile, tile.IsToggled);
