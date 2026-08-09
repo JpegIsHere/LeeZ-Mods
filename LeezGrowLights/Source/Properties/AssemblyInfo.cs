5ce7854f65f2b0e0c6657920f356cf33eae8009d
JpegIsHere <136973495+JpegIsHere@users.noreply.github.com>
2026-08-09 14:49:22 +1000

    release: bump assembly version to 0.7.0.2

--- LeezGrowLights/Source/Properties/AssemblyInfo.cs
+++ LeezGrowLights/Source/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("LeezGrowLights")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b31ad3c2-9a13-47b4-954e-c21f9d6d0bb2")]
-[assembly: AssemblyVersion("0.7.0.1")]
-[assembly: AssemblyFileVersion("0.7.0.1")]
+[assembly: AssemblyVersion("0.7.0.2")]
+[assembly: AssemblyFileVersion("0.7.0.2")]
